6cafb3062b11647585975ae4d5e9fbe0a8bd3d1e
s3rper <113769202+s3rper@users.noreply.github.com>
2022-10-11 12:08:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-p9es24
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-p9es24)
---
+++ index.js
@@ -0,0 +1,5 @@
+const Webflow = require('webflow-api');
+
+const api = new Webflow({ token: 'dd4183c797cff7d99f7bb50971137e4ffbb0152c0816097115030dcb84ee96cf' });
+
+api.sites();
